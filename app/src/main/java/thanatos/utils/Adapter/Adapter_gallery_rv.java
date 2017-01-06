3f4ac83b161c0b2deab95f22c4ccda72a1150b5d
git <523928638@qq.com>
2017-01-06 17:24:57 +0800

    tianjiaxiangce

--- app/src/main/java/thanatos/utils/Adapter/Adapter_gallery_rv.java
+++ app/src/main/java/thanatos/utils/Adapter/Adapter_gallery_rv.java
@@ -2,6 +2,8 @@ package thanatos.utils.Adapter;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.List;
 import thanatos.utils.R;
 import thanatos.utils.base.BaseRVAdapter;
 import thanatos.utils.entity.PicInfo;
-import thanatos.utils.utils.ImageLoader;
+import thanatos.utils.utils.imagecachetool.ImageLoader;
 
 /**
  * Created by lxf52 on 2017/1/5.
@@ -30,18 +32,27 @@ public class Adapter_gallery_rv extends BaseRVAdapter {
 
     @Override
     public View getHeader() {
-        return View.inflate(context, R.layout.item_gallery_rv,null);
+        return null;
     }
 
     @Override
     public View getFooter() {
-        return View.inflate(context, R.layout.item_gallery_rv,null);
+        return null;
     }
 
     @Override
-    public void setData(MyRecyclerViewHolder holder, int position, Object data) {
-        PicInfo picInfo= (PicInfo) data;
-        ImageLoader.getInstance(picInfo.getUrl(), (ImageView) holder.setView(R.id.iv_item_gallery_rv));
+    public void setData(final MyRecyclerViewHolder holder, int position, Object data) {
+        final PicInfo picInfo= (PicInfo) data;
+       ImageLoader.getInstance().loadImage(picInfo.getUrl(), (ImageView) holder
+                .setView(R.id.iv_item_gallery_rv));
+        final CheckBox cb=holder.setView(R.id.rb_item_gallery_rv);
+        cb.setChecked(picInfo.isChecked());
+        cb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                picInfo.setChecked(cb.isChecked());
+            }
+        });
 
     }
 }
